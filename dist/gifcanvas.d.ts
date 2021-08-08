bb9df589495051323be76188b0f7ba575ae1cacd
FlexICE <two.fl3xice@yandex.ru>
2021-08-08 21:58:38 +0400

    delete dists

--- dist/gifcanvas.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { Canvas, NodeCanvasRenderingContext2D } from "canvas";
-export declare class GifCanvas {
-    canvas: Canvas;
-    private encoder;
-    constructor(canvas: Canvas, delay?: number, fps?: number, repeat?: number);
-    addFrame(fn: (ctx: NodeCanvasRenderingContext2D) => void): this;
-    end(): Buffer;
-}
